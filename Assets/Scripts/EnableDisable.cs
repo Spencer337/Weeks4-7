585b4b80535236b7a39462ce2233cb76479cd8d5
Spencer Walter <waltespe@shernet.sheridancollege.ca>
2025-01-29 10:49:35 -0500

    Made some noise

--- Assets/Scripts/EnableDisable.cs
+++ Assets/Scripts/EnableDisable.cs
@@ -7,6 +7,8 @@ public class EnableDisable : MonoBehaviour
     public SpriteRenderer sr;
     public EnableDisable script;
     public GameObject go;
+    public AudioSource audioSource;
+    public AudioClip clip;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +29,15 @@ public class EnableDisable : MonoBehaviour
             //sr.enabled = true;
             //script.enabled = true;
             go.SetActive(true);
+        }
+       if (Input.GetKey(KeyCode.Space))
+        {
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.PlayOneShot(clip);
+                //    audioSource.Play();
+            }
+            
         }
     }
 }
